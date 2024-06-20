0b3bb052744f1d3f17676201da35c5c34d7536e8
srikanthyaswada <srikanthyaswada.vita@gmail.com>
2024-06-20 11:37:03 +0530

    login page completed

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './admin/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', title: 'admin-login', component: LoginComponent },
+  { path: 'login', title: 'admin-login', component: LoginComponent },
+];
